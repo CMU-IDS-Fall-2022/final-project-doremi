61942c697eb61ead70cc67bf799f52bb8f5ab40e
Tomas <tcabezon@andrew.cmu.edu>
2022-11-18 14:42:06 -0500

    changes

--- get_data_from_jsons.py
+++ get_data_from_jsons.py
@@ -7,53 +7,117 @@ path = "json_files"
 files = os.listdir(path)
  
 print("Files and directories in '", path, "' :",files)
-
+files.sort()
+print('files->',files)
 songs=0
-header=['title','uri','name','playcount','trackNumber','duration','artist','other artist']
+artists=0
+header=['name','bio','linkName','linkUrl','monthlyListeners','worldRank','L0_city','L0_country','L0_numberOfListeners','L0_region',
+'L1_city','L1_country','L1_numberOfListeners','L1_region',
+'L2_city','L2_country','L2_numberOfListeners','L2_region',
+'L3_city','L3_country','L3_numberOfListeners','L3_region',
+'L4_city','L4_country','L4_numberOfListeners','L4_region']
+
 rows=[]
-track=[]
-for file in files:
-    title=file.split('.')[0]
+# track=[]
+artist=[]
+for file in files[1:]:
+
+    print('-----------------------------')
     print('--->'+file)
-    print('--->TITLE',title)
+    print('-----------------------------')
     f = open(path+'/'+file)
     jsonObj=json.load(f)
-    # print(jsonObj)
-    # print(type(jsonObj))
-    for i,items in enumerate(jsonObj['data']['album']['tracks']['items']):
+    
+    for i,items in enumerate(jsonObj['data']['artist']):
         
-        print(' --> track',i)
-        print('  -> title',title)
-        track.append(title)
-        for val in items['track']:
-            if val in ['uri','name','playcount','trackNumber','duration','artists']:
-                
-                if val=='duration':
-                    print('  ->',val,items['track'][val]['totalMilliseconds'])
-                    track.append(items['track'][val]['totalMilliseconds'])
-                elif val=='artists':
-                    if len(items['track'][val]['items'])==1: #only rosalia
-                        print('  ->artist  ',items['track'][val]['items'][0]['profile']['name'])
-                        print('  -> other artists  ',None)
-                        track.append(items['track'][val]['items'][0]['profile']['name'])
-                        track.append(None)
-                    else:
-                        print('  ->artist  ',items['track'][val]['items'][0]['profile']['name'])
-                        track.append(items['track'][val]['items'][0]['profile']['name'])
-                        others=[]
-                        for a,artist in enumerate( items['track'][val]['items'][1:]):
-                            print('  -> other artist  ',a,artist['profile']['name'])
-                            others.append(artist['profile']['name'])
-                        track.append((','.join(others)))
-                else: 
-                    print('  ->',val,items['track'][val])
-                    track.append(items['track'][val])
-        songs+=1
-        print(track)
-        rows.append(track)
-        track=[]
 
-print('total songs',songs)
+        if items=='profile':
+            print('->name',jsonObj['data']['artist']['profile']['name'])
+            print('->bio',jsonObj['data']['artist']['profile']['biography']['text'])
+
+            artist.append(jsonObj['data']['artist']['profile']['name'])
+            artist.append(jsonObj['data']['artist']['profile']['biography']['text'])
+
+            try:
+                print('->linkName',jsonObj['data']['artist']['profile']['externalLinks']['items'][0]['name'])
+                print('->linkUrl',jsonObj['data']['artist']['profile']['externalLinks']['items'][0]['url'])
+                artist.append(jsonObj['data']['artist']['profile']['externalLinks']['items'][0]['name'])
+                artist.append(jsonObj['data']['artist']['profile']['externalLinks']['items'][0]['url'])
+            except:
+                artist.append('None')
+                artist.append('None')
+
+
+            
+            
+        if items=='stats':
+            print('->monthlyListeners',jsonObj['data']['artist']['stats']['monthlyListeners'])
+            print('->worldRank',jsonObj['data']['artist']['stats']['worldRank'])
+            artist.append(jsonObj['data']['artist']['stats']['monthlyListeners'])
+            artist.append(jsonObj['data']['artist']['stats']['worldRank'])
+            print('')
+
+            print('->L0_city',jsonObj['data']['artist']['stats']['topCities']['items'][0]['city'])
+            print('->L0_country',jsonObj['data']['artist']['stats']['topCities']['items'][0]['country'])
+            print('->L0_numberOfListeners',jsonObj['data']['artist']['stats']['topCities']['items'][0]['numberOfListeners'])
+            print('->L0_region',jsonObj['data']['artist']['stats']['topCities']['items'][0]['region'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][0]['city'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][0]['country'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][0]['numberOfListeners'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][0]['region'])
+            print('')
+
+            print('->L1_city',jsonObj['data']['artist']['stats']['topCities']['items'][1]['city'])
+            print('->L1_country',jsonObj['data']['artist']['stats']['topCities']['items'][1]['country'])
+            print('->L1_numberOfListeners',jsonObj['data']['artist']['stats']['topCities']['items'][1]['numberOfListeners'])
+            print('->L1_region',jsonObj['data']['artist']['stats']['topCities']['items'][1]['region'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][1]['city'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][1]['country'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][1]['numberOfListeners'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][1]['region'])
+            print('')
+
+            print('->L2_city',jsonObj['data']['artist']['stats']['topCities']['items'][2]['city'])
+            print('->L2_country',jsonObj['data']['artist']['stats']['topCities']['items'][2]['country'])
+            print('->L2_numberOfListeners',jsonObj['data']['artist']['stats']['topCities']['items'][2]['numberOfListeners'])
+            print('->L2_region',jsonObj['data']['artist']['stats']['topCities']['items'][2]['region'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][2]['city'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][2]['country'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][2]['numberOfListeners'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][2]['region'])
+            print('')
+
+            print('->L3_city',jsonObj['data']['artist']['stats']['topCities']['items'][3]['city'])
+            print('->L3_country',jsonObj['data']['artist']['stats']['topCities']['items'][3]['country'])
+            print('->L3_numberOfListeners',jsonObj['data']['artist']['stats']['topCities']['items'][3]['numberOfListeners'])
+            print('->L3_region',jsonObj['data']['artist']['stats']['topCities']['items'][3]['region'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][3]['city'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][3]['country'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][3]['numberOfListeners'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][3]['region'])
+            print('')
+
+            print('->L4_city',jsonObj['data']['artist']['stats']['topCities']['items'][4]['city'])
+            print('->L4_country',jsonObj['data']['artist']['stats']['topCities']['items'][4]['country'])
+            print('->L4_numberOfListeners',jsonObj['data']['artist']['stats']['topCities']['items'][4]['numberOfListeners'])
+            print('->L4_region',jsonObj['data']['artist']['stats']['topCities']['items'][4]['region'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][4]['city'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][4]['country'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][4]['numberOfListeners'])
+            artist.append(jsonObj['data']['artist']['stats']['topCities']['items'][4]['region'])
+            print('')
+
+    rows.append(artist)
+    artists+=1
+    print(artist)
+    print('len(artist)',len(artist))
+    print('')
+    artist=[]
+
+print('')
+print('headers',header)
+print(len(header))
+print('total artists',artists)
 print('data len',len(rows))
 
 with open('spotify_data.csv', 'w') as f:
@@ -64,7 +128,7 @@ with open('spotify_data.csv', 'w') as f:
     write.writerow(header)
     write.writerows(rows)
 
-# print(','.join(['hola','adios'])    )
+
         
 
         
